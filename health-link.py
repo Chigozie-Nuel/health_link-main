91080f24277c5b3fd39cea4ce0d4966e4a4c858b
Chigozie <c.ndubuaku@alustudent.com>
2025-09-25 22:19:41 +0400

    Create health-link.py

---
+++ health-link.py
@@ -0,0 +1,14 @@
+import uuid
+import qrcode
+from datetime import datetime
+
+# Predefined medical departments
+MEDICAL_DEPARTMENTS = {
+    "1": "General Practitioner",
+    "2": "Dentist",
+    "3": "Surgeon",
+    "4": "Pediatrician",
+    "5": "Cardiologist",
+    "6": "Nurse",
+    "7": "Admin"
+}
